6d2d072e8d0b9de17e808413747fd2689b5f5987
Andrew Njoo <adnjoo@gmail.com>
2021-08-07 12:28:49 -0700

    add dashboard

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -6,7 +6,12 @@ router.get('/', authorization, async (req,res)=>{
     try {
 
         //after passing middleware, req.user has payload
+        // res.json(req.user)
 
+        const user = await pool.query("SELECT user_name FROM users WHERE user_id =$1", 
+        [req.user])
+
+        res.json(user.rows[0]);
     } catch (err) {
         console.log(err.message)
         res.status(500).json('server error')
