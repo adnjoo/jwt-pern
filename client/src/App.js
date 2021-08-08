84b24f0ff0bcd2bf6647eafba44761767e16a38b
Andrew Njoo <adnjoo@gmail.com>
2021-08-07 18:54:08 -0700

    edit

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,65 @@
-import React, {Fragment} from "react";
+import React, { Fragment, useState } from "react";
 
-import {BrowserRouter as Router,
-   Switch,
-   Route,
-   Redirect
-  } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
-import {Container} from 'react-bootstrap'
+import { Container } from "react-bootstrap";
 
 //Components
-import Dashboard from './components/Dashboard'
-import Login from './components/Login'
-import Register from './components/Register'
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
+  const [isAuthenticated, setisAuthenticated] = useState(false);
+
+  const setAuth = boolean => {
+    setisAuthenticated(boolean)
+  }
+
   return (
     <Fragment>
       <Router>
         <Container>
-        <Switch>
-          <Route exact path ='/login' render={props=> <Login {...props}/>}/>
-          <Route exact path ='/register' render={props=> <Register {...props}/>}/>
-          <Route exact path ='/dashboard' render={props=> <Dashboard {...props}/>}/>
-        </Switch>
+          <Switch>
+            <Route
+              exact
+              path="/login"
+              render={(props) =>
+                !isAuthenticated ? (
+                  <Login {...props} setAuth={setAuth} />
+                ) : (
+                  <Redirect to="/dashboard" />
+                )
+              }
+            />
+            <Route
+              exact
+              path="/register"
+              render={(props) =>
+                !isAuthenticated ? (
+                  <Register {...props} setAuth={setAuth} />
+                ) : (
+                  <Redirect to="/login" />
+                )
+              }
+            />
+            <Route
+              exact
+              path="/dashboard"
+              render={(props) =>
+                isAuthenticated ? (
+                  <Dashboard {...props} setAuth={setAuth} />
+                ) : (
+                  <Redirect to="/login" />
+                )
+              }
+            />
+          </Switch>
         </Container>
       </Router>
     </Fragment>
